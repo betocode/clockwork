ffdeebddda937794b3f45635debe31d742faf573
roberto <robertojr_@outlook.com.br>
2022-06-28 08:35:01 -0300

    not found page add

--- src/main/routes/routes.tsx
+++ src/main/routes/routes.tsx
@@ -1,7 +1,5 @@
-import React, { useContext } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MovieFilterContext from "../../presentation/context/movieFilterContext";
-import { MovieDetail } from "../../presentation/pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NotFound } from "../../presentation/pages";
 import MakeHome from "../factories/pages/makeHome";
 import MakeMovieDetail from "../factories/pages/makeMovieDetail";
 
@@ -10,20 +8,11 @@ const AppRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/moviedetail/:id" element={<MakeMovieDetail />} />
-        <Route path="*" element={<MyComponent text={"not found"} />} />
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<MakeHome />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-type Props = {
-  text: string;
-};
-const MyComponent: React.FC<Props> = ({ text }: { text: string }) => {
-  const ctx = useContext(MovieFilterContext);
-
-  return <div>{text}</div>;
 };
 
 export default AppRoutes;
--- src/presentation/pages/index.ts
+++ src/presentation/pages/index.ts
@@ -2,3 +2,4 @@
 
 export {default as Home} from './home'
 export {default as MovieDetail} from './movieDetail'
+export {default as NotFound} from './notFound'
